39c94826b8f53f64a94d8f2c5dc519484f7fd32f
Malefic <ogupta4242@gmail.com>
2024-09-11 15:55:58 -0700

    Added photon vision

--- src/main/java/frc/robot/subsystems/Photonvision.java
+++ src/main/java/frc/robot/subsystems/Photonvision.java
@@ -1,18 +1,78 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems;
 
+import java.util.Optional;
+
+import org.photonvision.EstimatedRobotPose;
+import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.PhotonPoseEstimator.PoseStrategy;
+import org.photonvision.targeting.PhotonTrackedTarget;
+
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Photonvision extends SubsystemBase {
-  /** Creates a new Photonvision. */
-  // TODO Get the photonvision from crescendo swerve subsystem and paste it all into here to be used in swervesubsystem
-  public Photonvision() {}
+/**
+ * The PhotonVision subsystem handles vision processing using PhotonVision cameras.
+ */
+public class PhotonVision extends SubsystemBase {
+  // PhotonVision cameras
+  PhotonCamera camera1 = new PhotonCamera("Camera One");
+  PhotonCamera camera2 = new PhotonCamera("Camera Two");
 
+  // Tracked targets from the cameras
+  PhotonTrackedTarget target1;
+  PhotonTrackedTarget target2;
+
+  // Pose estimator for determining the robot's position on the field
+  PhotonPoseEstimator photonPoseEstimator;
+
+  // AprilTag field layout for the 2024 Crescendo field
+  AprilTagFieldLayout aprilTagFieldLayout = AprilTagFields.k2024Crescendo.loadAprilTagLayoutField();
+
+  // Transformation from the robot to the camera
+  Transform3d robotToCam = new Transform3d(new Translation3d(0.5, 0.0, 0.5), new Rotation3d(0,0,0)); // Cam mounted facing forward, half a meter forward of center, half a meter up from center.
+
+  /**
+   * Constructs a new PhotonVision subsystem.
+   */
+  public PhotonVision() {
+    photonPoseEstimator = new PhotonPoseEstimator(aprilTagFieldLayout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, camera1, robotToCam);
+  }
+
+  /**
+   * This method is called periodically by the scheduler.
+   * It updates the tracked targets and displays relevant information on the SmartDashboard.
+   */
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    var result1 = camera1.getLatestResult();
+    var result2 = camera2.getLatestResult();
+
+    if (result1.hasTargets()){
+      target1 = result1.getBestTarget();
+      if (result1.getMultiTagResult().estimatedPose.isPresent) {
+        Transform3d fieldToCamera = result1.getMultiTagResult().estimatedPose.best;
+        SmartDashboard.putNumber("field to camera", fieldToCamera.getX());
+      }
+    }
+
+    target2 = result2.hasTargets() ? result2.getBestTarget() : target2;
+  }
+
+  /**
+   * Gets the estimated global pose of the robot.
+   *
+   * @param prevEstimatedRobotPose The previous estimated pose of the robot.
+   * @return An Optional containing the estimated robot pose, or empty if no pose could be estimated.
+   */
+  public Optional<EstimatedRobotPose> getEstimatedGlobalPose(Pose2d prevEstimatedRobotPose) {
+    photonPoseEstimator.setReferencePose(prevEstimatedRobotPose);
+    return photonPoseEstimator.update();
   }
 }
