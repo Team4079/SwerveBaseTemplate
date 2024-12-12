711d6a7695f1d64fcf310a9ecc7bb473b3827407
Malefic <om@malefic.xyz>
2024-12-12 15:56:11 -0800

    removal(advatnagekit): remove advantagekit from the code
    Removed advtange kit in order to test swerve base due to issues with compilation.

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -5,10 +5,10 @@ import edu.wpi.first.wpilibj.RobotBase;
 public final class Main {
   /**
    * Good luck if you change any of this!
-   * <p>
-   * Hello!
-   * <p>
-   * Help I'm stuck in the basement.
+   *
+   * <p>Hello!
+   *
+   * <p>Help I'm stuck in the basement.
    */
   public static void main(String... args) {
     RobotBase.startRobot(Robot::new);
